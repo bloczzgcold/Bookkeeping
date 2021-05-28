a68b32c4cd92df2a2b07bdb6abbd0faa25555bc2
bloczzgcold <931116766@qq.com>
2021-05-28 16:38:39 +0800

    app

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Bookkeeping
 消费记账，团队、个人费用分摊
+
+[参考：https://github.com/KunMinX/Jetpack-MVVM-Best-Practice]: 
+
--- app/src/main/java/com/bloczzgcold/bookkeeping/App.java
+++ app/src/main/java/com/bloczzgcold/bookkeeping/App.java
@@ -5,8 +5,15 @@ import android.content.Context;
 import androidx.multidex.MultiDex;
 
 import com.bloczzgcold.core.base.BaseApplication;
+import com.bloczzgcold.core.base.utils.Utils;
 
 public class App extends BaseApplication {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Utils.init(this);
+    }
 
     @Override
     protected void attachBaseContext(Context base) {
